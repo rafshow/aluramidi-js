4383f809b95cda8fc5aa42164c26a6e87c7c4310
Rafael <krafaelcoelho@gmail.com>
2023-04-09 04:05:53 -0300

    primeira experiencia com js

---
+++ main.js
@@ -0,0 +1,19 @@
+
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listadeTeclas = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < listadeTeclas.length; i++) {
+    const tecla = listadeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    //console.log(idAudio);
+    tecla.onclick =  function () {
+        tocaSom(idAudio)
+    }
+    //console.log(i);  
+}
+
+
